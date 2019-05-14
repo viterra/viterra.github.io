b020aaa45088863ccccb9747fa0ecd68868682d4
Zheka-ded <41625614+Zheka-ded@users.noreply.github.com>
2019-05-15 02:20:17 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,106 +1,68 @@
+// var btn1 = document.getElementById(`addNotif`);
+
+// var btnActive = btn1.querySelector(`.notifications-menu__inputSearch-wrap`);
+
+// btn1.onclick = function () {
+//     btn1.classList.toggle(`notifications-menu__inputSearch-wrapActive`);
+// };
 
 
 
 
 
-// (function () {
-        
-//     const newNotif = document.querySelector(`.notifications-menu__addNotif`);
-//     const actievSearch = document.querySelector(`.notifications-menu__inputSearch-wrap`);
+let activeButton = document.getElementById(`regMenu`);
+let activeUserSvg = document.querySelector(`.headerVSRegForm__svgUser`);
+let activeArrowSvg = document.querySelector(`.headerVSRegForm__svgRegArrow`);
+let activeForm = document.querySelector(`.headerVSRegForm__form-wrap`);
 
-//     newNotif.addEventListener(`click`, function(w) {
-//         w.preventDefault();
-//             if (actievSearch.classList.contains(`notifications-menu__inputSearch-wrapActive`)) {
-//                 actievSearch.classList.remove(`notifications-menu__inputSearch-wrapActive`);
-//             } else {
-//                 actievSearch.classList.add(`notifications-menu__inputSearch-wrapActive`);
-//             }
-
-//     });
-// })();
+activeButton.onclick = function () {
+    this.classList.toggle(`headerVSRegForm__registrationMenuActive`);
+    activeUserSvg.classList.toggle(`headerVSRegForm__svgUserActive`);
+    activeArrowSvg.classList.toggle(`headerVSRegForm__svgRegArrowActive`);
+    activeForm.classList.toggle(`headerVSRegForm__form-wrapActive`);
+};
 
 
 
 
 
-// const first = function () {
-        
-
-//     const newNotif = document.querySelector(`.notifications-menu__addNotif`);
-//     const actievSearch = document.querySelector(`.notifications-menu__inputSearch-wrap`);
-
-//     newNotif.addEventListener(`click`, function(w) {
-//         w.preventDefault();
-//             if (actievSearch.classList.contains(`notifications-menu__inputSearch-wrapActive`)) {
-//                 actievSearch.classList.remove(`notifications-menu__inputSearch-wrapActive`);
-//             } else {
-//                 actievSearch.classList.add(`notifications-menu__inputSearch-wrapActive`);
-//             }
-
-//     });
-// }();
-
-// const activeButton = document.querySelector(`.headerVSRegForm__registrationMenu`);
-// const activeUserSvg = document.querySelector(`.headerVSRegForm__svgUser`);
-// const activeArrowSvg = document.querySelector(`.headerVSRegForm__svgRegArrow`);
-// const activeForm = document.querySelector(`.headerVSRegForm__form-wrap`);
-
-// // activeButton.addEventListener(`click`, function(e){
-// //     e.preventDefault();
-// //     // this.classList.toggle(`is__active`);
-// //     if (this.classList.contains(`headerVSRegForm__registrationMenuActive`)) {
-// //         this.classList.remove(`headerVSRegForm__registrationMenuActive`);
-// //         activeUserSvg.classList.remove(`headerVSRegForm__svgUserActive`);
-// //         activeArrowSvg.classList.remove(`headerVSRegForm__svgRegArrowActive`);
-// //         activeForm.classList.remove(`headerVSRegForm__form-wrapActive`);
-// //     } else {
-// //         this.classList.add(`headerVSRegForm__registrationMenuActive`);
-// //         activeUserSvg.classList.add(`headerVSRegForm__svgUserActive`);
-// //         activeArrowSvg.classList.add(`headerVSRegForm__svgRegArrowActive`);
-// //         activeForm.classList.add(`headerVSRegForm__form-wrapActive`);
-// //     }
-// // });
-
-
-// activeButton.addEventListener(`click`, function(){
-//     // e.preventDefault();
-//     // this.classList.toggle(`is__active`);
-//     if (this.classList.contains(`headerVSRegForm__registrationMenuActive`)) {
-//         this.classList.remove(`headerVSRegForm__registrationMenuActive`);
-//         activeUserSvg.classList.remove(`headerVSRegForm__svgUserActive`);
-//         activeArrowSvg.classList.remove(`headerVSRegForm__svgRegArrowActive`);
-//         activeForm.classList.remove(`headerVSRegForm__form-wrapActive`);
-//     } else {
-//         this.classList.add(`headerVSRegForm__registrationMenuActive`);
-//         activeUserSvg.classList.add(`headerVSRegForm__svgUserActive`);
-//         activeArrowSvg.classList.add(`headerVSRegForm__svgRegArrowActive`);
-//         activeForm.classList.add(`headerVSRegForm__form-wrapActive`);
-//     }
-// });
 
 
 
-const second = function () {
 
-    const activeButton = document.querySelector(`.headerVSRegForm__registrationMenu`);
-    const activeUserSvg = document.querySelector(`.headerVSRegForm__svgUser`);
-    const activeArrowSvg = document.querySelector(`.headerVSRegForm__svgRegArrow`);
-    const activeForm = document.querySelector(`.headerVSRegForm__form-wrap`);
-    
-    activeButton.addEventListener(`click`, function(e){
-        e.preventDefault();
-        // this.classList.toggle(`is__active`);
-        if (this.classList.contains(`headerVSRegForm__registrationMenuActive`)) {
-            this.classList.remove(`headerVSRegForm__registrationMenuActive`);
-            activeUserSvg.classList.remove(`headerVSRegForm__svgUserActive`);
-            activeArrowSvg.classList.remove(`headerVSRegForm__svgRegArrowActive`);
-            activeForm.classList.remove(`headerVSRegForm__form-wrapActive`);
-        } else {
-            this.classList.add(`headerVSRegForm__registrationMenuActive`);
-            activeUserSvg.classList.add(`headerVSRegForm__svgUserActive`);
-            activeArrowSvg.classList.add(`headerVSRegForm__svgRegArrowActive`);
-            activeForm.classList.add(`headerVSRegForm__form-wrapActive`);
-        }
-    });
 
-}();
+
+// var btn1 = document.getElementById(`addNotif`);
+
+// var btnActive = btn1.querySelector(`.notifications-menu__inputSearch-wrap`);
+
+// btn1.onclick = function () {
+//     btn1.classList.toggle(`notifications-menu__inputSearch-wrapActive`);
+// };
+
+
+
+var btn1 = document.getElementById(`addNotif`);
+
+var btnActive = document.querySelector(`.notifications-menu__inputSearch-wrap`);
+
+btn1.onclick = function () {
+    btnActive.classList.toggle(`notifications-menu__inputSearch-wrapActive`);
+};
+
+
+
+
+
+
+// let btn2 = document.getElementById(`regMenu`);
+// let activeUserSvg = btn2.querySelector(`.headerVSRegForm__svgUser`);
+// let activeArrowSvg = btn2.querySelector(`.headerVSRegForm__svgRegArrow`);
+// let activeForm = btn2.querySelector(`.headerVSRegForm__form-wrap`);
+
+// btn2.onclick = function () {
+//     this.classList.toggle(`headerVSRegForm__registrationMenuActive`);
+//     activeUserSvg.classList.toggle(`headerVSRegForm__svgUserActive`);
+//     activeArrowSvg.classList.toggle(`headerVSRegForm__svgRegArrowActive`);
+//     activeForm.classList.toggle(`headerVSRegForm__form-wrapActive`);
+// };
