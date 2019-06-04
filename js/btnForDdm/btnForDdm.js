c5a510a4452662240e171c8f438f3e7c9eec86a8
Zheka-ded <41625614+Zheka-ded@users.noreply.github.com>
2019-06-05 00:19:06 +0300

    Add files via upload

--- js/btnForDdm/btnForDdm.js
+++ js/btnForDdm/btnForDdm.js
@@ -1,12 +1,10 @@
 const btnMenu = document.querySelector(`.hamburger`);
 const ddMenu = document.querySelector(`.header__nav`);
 const zIndexNotif = document.querySelector(`.header__btnForNotif`);
-// const zIndezRegForm = document.querySelector(`.headerVSRegForm__registrationMenu`);
 
 btnMenu.addEventListener(`click`, function (e) {
     e.preventDefault();
     this.classList.toggle(`isActive`);
     ddMenu.classList.toggle(`header__navActive`);
     zIndexNotif.classList.toggle(`header__zIndexNotif`);
-    // zIndezRegForm.classList.toggle(`headerVSRegForm__zIndexRegForm`);
 });
--- js/headerVSRegForm/headerVSRegForm.js
+++ js/headerVSRegForm/headerVSRegForm.js
@@ -1,10 +1,12 @@
 const btnMenu = document.querySelector(`.hamburger`);
 const ddMenu = document.querySelector(`.headerVSRegForm__nav`);
+const zIndezRegForm = document.querySelector(`.headerVSRegForm__registrationMenu`);
 
 btnMenu.addEventListener(`click`, function (e) {
     e.preventDefault();
     this.classList.toggle(`isActive`);
     ddMenu.classList.toggle(`headerVSRegForm__navActive`);
+    zIndezRegForm.classList.toggle(`headerVSRegForm__zIndexRegForm`);
 });
 
 
