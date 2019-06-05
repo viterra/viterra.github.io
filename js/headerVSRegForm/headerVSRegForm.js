f9a4caab253fc639606d28ffbf3ea5ecea80bf01
Zheka-ded <41625614+Zheka-ded@users.noreply.github.com>
2019-06-05 23:13:37 +0300

    Add files via upload

--- js/headerVSRegForm/headerVSRegForm.js
+++ js/headerVSRegForm/headerVSRegForm.js
@@ -9,18 +9,3 @@ btnMenu.addEventListener(`click`, function (e) {
     zIndezRegForm.classList.toggle(`headerVSRegForm__zIndexRegForm`);
 });
 
-
-// const activeButton = document.getElementById(`regMenu`);
-// const activeUserSvg = document.querySelector(`.headerVSRegForm__svgUser`);
-// const activeArrowSvg = document.querySelector(`.headerVSRegForm__svgRegArrow`);
-// const activeForm = document.querySelector(`.headerVSRegForm__form-wrap`);
-// const zIndexDdm = document.querySelector(`.header__btnForDdm`);
-
-// activeButton.onclick = function () {
-//     this.classList.toggle(`headerVSRegForm__registrationMenuActive`);
-//     activeUserSvg.classList.toggle(`headerVSRegForm__svgUserActive`);
-//     activeArrowSvg.classList.toggle(`headerVSRegForm__svgRegArrowActive`);
-//     activeForm.classList.toggle(`headerVSRegForm__form-wrapActive`);
-//     zIndexDdm.classList.toggle(`header__zIndexDdm`);
-// };
-
